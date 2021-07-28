37190bd84c73de786baf3f42bf9ee18589fe7f91
STS532-SureshPrabhu <sathish.selvaraj@hotmail.com>
2021-07-28 11:23:37 +0530

    newchanges

--- WebApi/WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/WebApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","suresh"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","suresh","abbas"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
